99f9a7f58e136927c5e075c5a2412a1321fd5d2b
Mikael Lyngvig <mikael@lyngvig.org>
2012-09-17 05:44:38 +0200

    Added: C# output from CodeGen so as to make things easier to understand

--- Beryl/CodeGen.cs
+++ Beryl/CodeGen.cs
@@ -20,33 +20,41 @@ namespace Beryl
 
         public void visit(AssignCommand that)
         {
-            Console.WriteLine("AssignCommand:");
-
-            /* generate code to do the assign command */
-            Console.WriteLine("name = {0}", that.Name);
-
+            Console.Write("{0} = ", that.Name);
             // we must explicitly walk all children of all nodes...
             that.Expression.visit(this);
+            Console.WriteLine(";");
         }
 
         public void visit(BeginCommand that)
         {
-            Console.WriteLine("BeginCommand:");
+            Console.WriteLine("{");
             that.Commands.visit(this);
+            Console.WriteLine("}");
         }
 
         public void visit(BooleanExpression that)
         {
+            Console.Write(that.Value ? "true" : "false");
         }
 
         public void visit(BooleanType that)
         {
+            Console.Write("bool");
         }
 
         public void visit(CallCommand that)
         {
-            Console.WriteLine("CallCommand:");
+            Console.Write("{0}(", that.Identifier);
+            foreach (Expression argument in that.Arguments)
+            {
+                argument.visit(this);
 
+                if (argument != that.Arguments[that.Arguments.Length - 1])
+                    Console.Write(", ");
+            }
+            Console.WriteLine(");");
+#if false
             switch (that.Identifier)
             {
                 case "getint":
@@ -66,6 +74,7 @@ namespace Beryl
 
             foreach (Expression argument in that.Arguments)
                 argument.visit(this);
+#endif
         }
 
         /* This is the second tree-walking method that is called! */
@@ -77,93 +86,130 @@ namespace Beryl
 
         public void visit(ConstantDeclaration that)
         {
-        }
-
-        public void visit(Declaration that)
-        {
+            Console.Write("const ");
+            that.Type.visit(this);
+            Console.Write(" {0} = ", that.Name);
+            that.Expression.visit(this);
+            Console.WriteLine(";");
         }
 
         public void visit(Declarations that)
         {
-        }
-
-        public void visit(Expression that)
-        {
+            foreach (Declaration declaration in that.DeclarationsArray)
+                declaration.visit(this);
         }
 
         public void visit(FunctionDeclaration that)
         {
             that.Type.visit(this);
+            Console.Write(" {0}(", that.Name);
             foreach (ParameterDeclaration parameter in that.Parameters)
+            {
                 parameter.visit(this);
+                if (parameter != that.Parameters[that.Parameters.Length - 1])
+                    Console.Write(", ");
+            }
+            Console.WriteLine(")");
+            Console.WriteLine("{");
+            Console.Write("return ");
             that.Body.visit(this);
+            Console.WriteLine(";");
+            Console.WriteLine("}");
         }
 
         public void visit(FunctionExpression that)
         {
+            Console.Write("{0}(", that.Name);
             foreach (Expression argument in that.Arguments)
+            {
                 argument.visit(this);
+                if (argument != that.Arguments[that.Arguments.Length - 1])
+                    Console.Write(", ");
+            }
+            Console.Write(")");
         }
 
         public void visit(IfCommand that)
         {
-            Console.WriteLine("IfCommand:");
+            Console.Write("if (");
             that.Expression.visit(this);
+            Console.WriteLine(")");
             that.If.visit(this);
+            Console.WriteLine("else");
             that.Else.visit(this);
         }
 
         public void visit(IntegerExpression that)
         {
+            Console.Write(that.Value);
         }
 
         public void visit(IntegerType that)
         {
-            // this method does nothing as long as we have only one type (Integer)
+            Console.Write("int");
         }
 
         public void visit(LetCommand that)
         {
-            Console.WriteLine("LetCommand:");
+            Console.WriteLine("{");
+            // note: function declarations should be output before the first line
             foreach (Declaration declaration in that.Declarations)
                 declaration.visit(this);
             that.Command.visit(this);
+            Console.WriteLine("}");
         }
 
         public void visit(ParameterDeclaration that)
         {
+            that.Type.visit(this);
+            Console.Write(" {0}", that.Name);
         }
 
         public void visit(ParenthesisExpression that)
         {
+            Console.Write("(");
+            that.Expression.visit(this);
+            Console.Write(")");
         }
 
         /* This is the first tree-walking method that is called! */
         public void visit(AST.Program that)
         {
-            /* process the Program node */
+            Console.WriteLine("namespace Foobar");
+            Console.WriteLine("{");
             that.Commands.visit(this);
+            Console.WriteLine("}");
         }
 
         public void visit(StringExpression that)
         {
+            Console.Write("{0}", that.Value);
         }
 
         public void visit(StringType that)
         {
+            Console.Write("string");
         }
 
         public void visit(VariableDeclaration that)
         {
+            that.Type.visit(this);
+            Console.WriteLine(" {0};", that.Name);
         }
 
+        /* VariableExpression perhaps ought to be called IdentifierExpression as it is used for both constants and variables. */
         public void visit(VariableExpression that)
         {
+            Console.Write(that.Name);
         }
 
         public void visit(WhileCommand that)
         {
-            Console.WriteLine("WhileCommand:");
+            Console.Write("while (");
+            that.Expression.visit(this);
+            Console.WriteLine(")");
+            that.Command.visit(this);
         }
     }
 }
+
